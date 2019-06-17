624c9ac65c7e628934fd99756f7d4d6b3502710a
katarina <roxyk07@gmail.com>
2019-06-17 17:52:05 +0300

    Homework#3 Cleaned up old code, moved from Maven to Gradle, added new test scenario

--- src/test/java/AnotepadTest.java
+++ src/test/java/AnotepadTest.java
@@ -1,22 +1,19 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
+import objects.NotePad;
+import org.junit.*;
+import io.qameta.allure.junit4.DisplayName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 
 public class AnotepadTest {
 
+    private static final String TITLE = "My new note";
+    private static final String NOTE = "Sample note";
+
     WebDriver driver;
-    WebDriverWait wait;
+    NotePad np;
 
     @BeforeClass
     public static void setupClass() {
@@ -26,7 +23,7 @@ public class AnotepadTest {
     @Before
     public void openBrowser() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 7);
+        np = new NotePad(driver);
     }
 
     @After
@@ -36,21 +33,25 @@ public class AnotepadTest {
 
     @Test
     public void testCreatingDeletingNote() {
-        driver.get("https://anotepad.com/");
+        np
+                .open()
+                .setTitle(TITLE)
+                .save();
 
-        driver.findElement(By.id("edit_title")).sendKeys("My New Note");
-        driver.findElement(By.id("btnSaveNote")).click();
+        assertEquals(TITLE, np.getTitleContent());
+    }
 
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".alert.alert-warning"), "You have saved your note as"));
+    @Test
+    @DisplayName("GL-523:F-50: Saving Private or Public notes by registered users")
+    public void testPrivateNote() {
+        np
+                .open()
+                .setTitle(TITLE)
+                .addContent(NOTE)
+                .save()
+                .privating()
+                .permitting();
 
-        driver.findElement(By.cssSelector(".delete")).click();
-
-        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-        driver.switchTo().alert();
-        alert.accept();
-
-        assertEquals("No note here.", driver.findElement(By.cssSelector(".saved_notes div")).getText());
-
-        driver.findElement(By.cssSelector(".saved_notes div")).getText();
+        assertEquals("Private Note", np.getPrivateNoteBtnName());
     }
 }
